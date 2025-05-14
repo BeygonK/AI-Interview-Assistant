33f521740e6527e14014f76e4f16e8ad3608fc53
BeygonK <kipkoechbeygon@gmail.com>
2025-05-14 22:45:49 +0300

    Implemented google auht

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,8 +1,22 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { supabase } from "@/services/supabaseClient";
 import Image from "next/image";
 import React from "react";
 
 function Login() {
+  // function to handle google login
+  const handleGoogleLogin = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+
+    if (error) {
+      console.error("Error logging in with Google:", error.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="flex flex-col items-center justify-center w-full max-w-md p-6 bg-white rounded-lg ">
@@ -14,8 +28,8 @@ function Login() {
             Welcome to <span className="text-blue-400">AI Interviewer</span>
           </h1>
           <p className="text-gray-600">Please log in to continue</p>
-          <Button className="mt-4">
-            <a href="/api/auth/signin">Login with Google</a>
+          <Button className="mt-4 cursor-pointer" onClick={handleGoogleLogin}>
+            Login with Google
           </Button>
         </div>
       </div>
